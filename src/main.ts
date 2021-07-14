66fdcb664a5794acb85bf5340edfcdc5786ee3a3
vinnym342 <vincentmarcos@hotmail.com>
2021-07-15 02:49:52 +0800

    Setup mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,34 @@
 import { scrapeUku } from "./scrape";
+import mongoose, { Schema, model } from "mongoose";
 
-export function init() {
-  scrapeUku();
+export async function init() {
+  mongoose.connect("mongodb://localhost:27017/uku", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", async function () {
+    console.log("cat made");
+
+    interface Cat {
+      name: string;
+    }
+    // 2. Create a Schema corresponding to the document interface.
+    const catSchema = new Schema<{ name: string }>({
+      name: { type: String, required: true },
+    });
+
+    const CatModel = model<Cat>("Cat", catSchema);
+
+    await CatModel.create({ name: "Niaomi" });
+
+    // pussy.save();
+
+    db.close();
+  });
+  // scrapeUku();
 }
 
 init();
